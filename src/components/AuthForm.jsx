18ebd5ec0ce335b7332a3f52db28b67f6142dd70
hyewon93 <baehw93@gmail.com>
2024-03-05 17:43:20 -0800

    Login with Google

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -22,7 +22,7 @@ const AuthForm = () => {
                     <Box flex={2} h={"1px"} bg={"gray.400"} />
                 </Flex>
 
-                <GoogleAuth />
+                <GoogleAuth prefix={isLogin ? "Log in" : "Sign up"} />
             </VStack>
         </Box>
         <Box border={"1px solid gray"} borderRadius={4} padding={5}>
